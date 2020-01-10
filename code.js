fa6afe78af03f95624f6bab4d27d403f6bfbca86
Ole Algoritme <olealgoritme@gmail.com>
2020-01-10 16:08:15 +0100

    update

--- code.js
+++ code.js
@@ -9,95 +9,102 @@ let answers = [];
 let quizes = [];
 
 class Quiz {
-    constructor(question, answers, correctAnswerIdx) {
-        if(typeof question !== 'string') 
-            throw Error("Question needs to be a type String");
-        else if (!Array.isArray(answers)) 
-            throw Error("Answers is not an array");
-        else if (typeof correctAnswerIdx !== 'number') 
-            throw Error("Correct answer does not point to an index of type Number");
-        this._question = question;
-        this._answers = answers;
-        this._correctAnswerIdx = correctAnswerIdx;
-    }
+  constructor(question, answers, correctAnswerIdx) {
+    if (typeof question !== "string")
+      throw Error("Question needs to be a type String");
+    else if (!Array.isArray(answers)) throw Error("Answers is not an array");
+    else if (typeof correctAnswerIdx !== "number")
+      throw Error("Correct answer does not point to an index of type Number");
+    this._question = question;
+    this._answers = answers;
+    this._correctAnswerIdx = correctAnswerIdx;
+  }
 
-    get question() {
-        return this._question;
-    }
-    
-    set question(questionStr) {
-        this._question = questionStr;
-    } 
+  get question() {
+    return this._question;
+  }
 
-    get answers() {
-        return this._answers;
-    }
-    
-    set answers(answersArray) {
-        this._answers = answersArray;
-    } 
+  set question(questionStr) {
+    this._question = questionStr;
+  }
 
-    get correctAnswerIdx() {
-        return this._correctAnswerIdx;
-    }
-    
-    set correctAnswerIdx(answerIdx) {
-        this._correctAnswerIdx = answerIdx;
-    } 
+  get answers() {
+    return this._answers;
+  }
 
-    correctAnswer() {
-        return this._answers[this._correctAnswerIdx];
-    }
+  set answers(answersArray) {
+    this._answers = answersArray;
+  }
+
+  get correctAnswerIdx() {
+    return this._correctAnswerIdx;
+  }
+
+  set correctAnswerIdx(answerIdx) {
+    this._correctAnswerIdx = answerIdx;
+  }
+
+  correctAnswer() {
+    return this._answers[this._correctAnswerIdx];
+  }
 }
 
 function printQuiz(quiz) {
-    console.log(
-    "Question: " + quiz.question + "\n"
-    "Answers: " + quiz.answers + "\n"
-    "Correct answer Index: " + quiz.correctAnswerIdx + "\n"
-    "Correct answer (String): " + quiz.correctAnswer());
+  console.log(
+    "Question: " +
+      quiz.question +
+      "\n" +
+      "Answers: " +
+      quiz.answers +
+      "\n" +
+      "Correct answer Index: " +
+      quiz.correctAnswerIdx +
+      "\n" +
+      "Correct answer (String): " +
+      quiz.correctAnswer()
+  );
 }
 
 function addQuizToHTML(quiz) {
-    let questionList = document.getElementById('question-list');
-    let questionContainer = document.createElement('div');
-    let questionText  = document.createElement('p');
-    
-    questionText.innerHTML = quiz.question;
-    questionContainer.appendChild(questionText);
+  let questionList = document.getElementById("question-list");
+  let questionContainer = document.createElement("div");
+  let questionText = document.createElement("p");
 
-    quiz.answers.forEach(function(answer) {
-        let answerButton = document.createElement('button');
-        answerButton.innerHTML = answer;
+  questionText.innerHTML = quiz.question;
+  questionContainer.appendChild(questionText);
 
-        // Add listeners for both correct and wrong answers
-        // Show appropriate alert message
-        if (answer === quiz.answers[quiz.correctAnswerIdx]) 
-            answerButton.addEventListener('click', function(e) {
-                alert('CORRECT!');
-            });
-        else 
-            answerButton.addEventListener('click', function(e) {
-                alert('WRONG!!');
-            });
+  quiz.answers.forEach(function(answer) {
+    let answerButton = document.createElement("button");
+    answerButton.innerHTML = answer;
 
-        questionContainer.appendChild(answerButton);
-    });
+    // Add listeners for both correct and wrong answers
+    // Show appropriate alert message
+    if (answer === quiz.answers[quiz.correctAnswerIdx])
+      answerButton.addEventListener("click", function(e) {
+        alert("CORRECT!");
+      });
+    else
+      answerButton.addEventListener("click", function(e) {
+        alert("WRONG!!");
+      });
 
-    questionList.appendChild(questionContainer);
+    questionContainer.appendChild(answerButton);
+  });
+
+  questionList.appendChild(questionContainer);
 }
 
 function resetState() {
-    let quiz = null; 
-    let question = null;
-    let correctAnswerIdx = 0;
-    let answers = [];
-    let quizes = [];
+  let quiz = null;
+  let question = null;
+  let correctAnswerIdx = 0;
+  let answers = [];
+  let quizes = [];
 }
 
 // #1 RGB question
 question = "What color is RGB 255,255,0 ?";
-answers = [ "Magenta", "Orange", "Red", "Blue" ];
+answers = ["Magenta", "Orange", "Red", "Blue"];
 correctAnswerIdx = 1; // index is 0-based
 quiz = new Quiz(question, answers, correctAnswerIdx);
 quizes.push(quiz);
@@ -105,15 +112,20 @@ resetState();
 
 // #2 Binary question
 question = "What is the decimal representation of binary 0000 0010 ?";
-answers = [ "1", "4", "8", "2" ];
+answers = ["1", "4", "8", "2"];
 correctAnswerIdx = 3; // index is 0-based
 quiz = new Quiz(question, answers, correctAnswerIdx);
 quizes.push(quiz);
 resetState();
 
-// #3 HTTPS question 
+// #3 HTTPS question
 question = "What is HTTPS ?";
-answers = [ "HTTPS is HTTP over TLS/SSL", "HTTPS is the same as HTTP but with cookies", "HTTPS does not exist", "HTTPS is a misspelled version of SHTTP" ];
+answers = [
+  "HTTPS is HTTP over TLS/SSL",
+  "HTTPS is the same as HTTP but with cookies",
+  "HTTPS does not exist",
+  "HTTPS is a misspelled version of SHTTP"
+];
 correctAnswerIdx = 0; // index is 0-based
 quiz = new Quiz(question, answers, correctAnswerIdx);
 quizes.push(quiz);
@@ -121,7 +133,7 @@ resetState();
 
 // #4 Math question
 question = "What is 8+8*(4-2)";
-answers = [ "64", "28", "32", "30" ];
+answers = ["64", "28", "32", "30"];
 correctAnswerIdx = 2; // index is 0-based
 quiz = new Quiz(question, answers, correctAnswerIdx);
 quizes.push(quiz);
@@ -129,6 +141,6 @@ resetState();
 
 // Show questions and answers
 quizes.forEach(function(quiz) {
-    addQuizToHTML(quiz);
-    printQuiz(quiz);
+  addQuizToHTML(quiz);
+  printQuiz(quiz);
 });
